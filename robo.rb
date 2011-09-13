a5e67cc2077776dfdeb4c13e8ad016cc53f6643f
Emery Miller <emery@opusworkspace.com>
2011-09-12 21:47:04 -0400

    my stuff

--- robo.rb
+++ robo.rb
@@ -8,31 +8,23 @@ r = Speedy.new(:x => 0, :y => 0, :direction => 0, :name => 'Sleeper') do
     end
   end
 
-r2 = Speedy.new(:x => 0, :y => 10, :direction => 0, :name => 'Killer') do
+r2 = Speedy.new(:x => 30, :y => 0, :direction => 0, :name => 'Killer') do
     while(true)
       shoot
     end
   end
 
-r3 = Speedy.new(:x => 0, :y => 0, :direction => 0, :name => 'Sleeper') do
+r3 = Speedy.new(:x => 0, :y => 30, :direction => 0, :name => 'Sleeper2') do
     while(true)
       wait
     end
   end
 
-r4 = Speedy.new(:x => 40, :y => 40, :direction => 0, :name => 'Emery') do
+r4 = Speedy.new(:x => 30, :y => 30, :direction => 0, :name => 'Emery') do
     while(true)
-      shoot
-      turn -1
-      move 1
-      turn 1
-      move 1
-      20.times { shoot }
-      turn -1
-      move 2
-      turn 1
-      move 2
-      20.times { shoot }
+      7.times { move 2; turn -1; move 2; turn 1} 
+      4.times { shoot; turn 1 }
+      4.times { shoot; turn 1 }
     end
   end
 
