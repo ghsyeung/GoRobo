b52ceeb01d4c4dde212b212999b88c91da718d73
Wild Rhombus <wildrhombus@gmail.com>
2011-09-12 21:50:21 -0400

    leslie li

--- robo.rb
+++ robo.rb
@@ -14,8 +14,17 @@ r2 = Speedy.new(:x => 0, :y => 10, :direction => 0, :name => 'Killer') do
     end
   end
 
+r3 = Speedy.new(:x => 30, :y => 0, :direction => 180, :name => 'Grrl') do
+    while(true)
+			move(1)
+			turn(-1)
+			shoot
+    end
+  end
+
+
 World.setup [
-  r, r2
+  r, r2, r3
 ]
 
 World.run
