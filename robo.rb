444b4f476a8fe71afc591bead66be6eb9ef8ad1d
Emery Miller <emery@opusworkspace.com>
2011-09-12 21:33:14 -0400

    Robo 4 is defined

--- robo.rb
+++ robo.rb
@@ -14,8 +14,32 @@ r2 = Speedy.new(:x => 0, :y => 10, :direction => 0, :name => 'Killer') do
     end
   end
 
+r3 = Speedy.new(:x => 0, :y => 0, :direction => 0, :name => 'Sleeper') do
+    while(true)
+      wait
+    end
+  end
+
+r4 = Speedy.new(:x => 40, :y => 40, :direction => 0, :name => 'Emery') do
+    while(true)
+      shoot
+      turn -1
+      move 1
+      turn 1
+      move 1
+      20.times { shoot }
+      turn -1
+      move 2
+      turn 1
+      move 2
+      20.times { shoot }
+    end
+  end
+
+
+
 World.setup [
-  r, r2
+  r, r2, r3, r4
 ]
 
 World.run
